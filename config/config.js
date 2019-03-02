d6796e8874058eafc211f00f216f2f3c9cf6e3b5
Quincy Iheme <qiheme@gmail.com>
2019-03-02 15:31:09 -0500

    Add workaround for using sequelize in heroku

---
+++ config/config.js
@@ -0,0 +1,27 @@
+// Since one of our properties needs to be snake cased,
+// we can disable the linting for that specific scenario
+// and line so that the build doesn't fail for a lint error
+
+module.exports = {
+  development: {
+    username: "root",
+    // make sure to set this env variable in your .env file
+    password: process.env.LOCAL_MYSQL_PASSWORD,
+    database: "exampledb",
+    host: "localhost",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "testdb",
+    host: "localhost",
+    dialect: "mysql",
+    logging: false
+  },
+  production: {
+    /* eslint-disable-next-line camelcase */
+    use_env_variable: JAWSDB_URL,
+    dialect: mysql
+  }
+};
