f264206af38ad6607320a0b5d3011bc136c7aa3e
Quincy Iheme <qiheme@gmail.com>
2019-03-02 11:24:42 -0500

    Add code with lint errors

--- models/example.js
+++ models/example.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
   var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+      text: DataTypes.STRING,
+    description: DatanhjbjTypes.TEXT
   });
   return Example;
 };
